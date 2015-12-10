2b48c77633063f11433044b52f7ef8a788a32a55
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2015-12-10 19:30:38 +0100

    Beamformer project template commit

---
+++ Beamformer/Beamformer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beamformer
+//
+//  Created by Cameron Palmer on 10.12.2015.
+//  Copyright © 2015 NTNU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
