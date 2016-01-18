b0aa5b467b8820aa8f14c50a288534fafd563da8
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-01-18 10:42:47 +0100

    Corrected the code to yield correct output

--- Beamformer/Beamformer/VerasonicsDelay.swift
+++ Beamformer/Beamformer/VerasonicsDelay.swift
@@ -15,52 +15,43 @@ class VerasonicsDelay: NSObject {
     private let samplingFrequencyHertz: Double = 7813000
     private let lensCorrection: Double = 14.14423409
     private let numberOfTransducerElements: Int = 192
+    private let numberOfActiveTransducerElements: Int = 128
     private let imageZStartInMM: Double = 0.0
     private let imageZStopInMM: Double = 50.0
 
     static let defaultDelays: [Double] = [
-        -19.10, -18.90, -18.70, -18.50, -18.30, -18.10,
-        -17.90, -17.70, -17.50, -17.30, -17.10, -16.90,
-        -16.70, -16.50, -16.30, -16.10, -15.90, -15.70,
-        -15.50, -15.30, -15.10, -14.90, -14.70, -14.50,
-        -14.30, -14.10, -13.90, -13.70, -13.50, -13.30,
-        -13.10, -12.90, -12.70, -12.50, -12.30, -12.10,
-        -11.90, -11.70, -11.50, -11.30, -11.10, -10.90,
-        -10.70, -10.50, -10.30, -10.10,  -9.90,  -9.70,
-        -9.50,  -9.30,  -9.10,  -8.90,  -8.70,  -8.50,
-        -8.30,  -8.10,  -7.90,  -7.70,  -7.50,  -7.30,
-        -7.10,  -6.90,  -6.70,  -6.50,  -6.30,  -6.10,
-        -5.90,  -5.70,  -5.50,  -5.30,  -5.10,  -4.90,
-        -4.70,  -4.50,  -4.30,  -4.10,  -3.90,  -3.70,
-        -3.50,  -3.30,  -3.10,  -2.90,  -2.70,  -2.50,
-        -2.30,  -2.10,  -1.90,  -1.70,  -1.50,  -1.30,
-        -1.10,  -0.90,  -0.70,  -0.50,  -0.30,  -0.10,
-        0.10,   0.30,   0.50,   0.70,   0.90,   1.10,
-        1.30,   1.50,   1.70,   1.90,   2.10,   2.30,
-        2.50,   2.70,   2.90,   3.10,   3.30,   3.50,
-        3.70,   3.90,   4.10,   4.30,   4.50,   4.70,
-        4.90,   5.10,   5.30,   5.50,   5.70,   5.90,
-        6.10,   6.30,   6.50,   6.70,   6.90,   7.10,
-        7.30,   7.50,   7.70,   7.90,   8.10,   8.30,
-        8.50,   8.70,   8.90,   9.10,   9.30,   9.50,
-        9.70,   9.90,  10.10,  10.30,  10.50,  10.70,
-        10.90,  11.10,  11.30,  11.50,  11.70,  11.90,
-        12.10,  12.30,  12.50,  12.70,  12.90,  13.10,
-        13.30,  13.50,  13.70,  13.90,  14.10,  14.30,
-        14.50,  14.70,  14.90,  15.10,  15.30,  15.50,
-        15.70,  15.90,  16.10,  16.30,  16.50,  16.70,
-        16.90,  17.10,  17.30,  17.50,  17.70,  17.90,
-        18.10,  18.30,  18.50,  18.70,  18.90,  19.10
+        -12.70, -12.50, -12.30, -12.10, -11.90, -11.70,
+        -11.50, -11.30, -11.10, -10.90, -10.70, -10.50,
+        -10.30, -10.10,  -9.90,  -9.70,  -9.50,  -9.30,
+         -9.10,  -8.90,  -8.70,  -8.50,  -8.30,  -8.10,
+         -7.90,  -7.70,  -7.50,  -7.30,  -7.10,  -6.90,
+         -6.70,  -6.50,  -6.30,  -6.10,  -5.90,  -5.70,
+         -5.50,  -5.30,  -5.10,  -4.90,  -4.70,  -4.50,
+         -4.30,  -4.10,  -3.90,  -3.70,  -3.50,  -3.30,
+         -3.10,  -2.90,  -2.70,  -2.50,  -2.30,  -2.10,
+         -1.90,  -1.70,  -1.50,  -1.30,  -1.10,  -0.90,
+         -0.70,  -0.50,  -0.30,  -0.10,   0.10,   0.30,
+          0.50,   0.70,   0.90,   1.10,   1.30,   1.50,
+          1.70,   1.90,   2.10,   2.30,   2.50,   2.70,
+          2.90,   3.10,   3.30,   3.50,   3.70,   3.90,
+          4.10,   4.30,   4.50,   4.70,   4.90,   5.10,
+          5.30,   5.50,   5.70,   5.90,   6.10,   6.30,
+          6.50,   6.70,   6.90,   7.10,   7.30,   7.50,
+          7.70,   7.90,   8.10,   8.30,   8.50,   8.70,
+          8.90,   9.10,   9.30,   9.50,   9.70,   9.90,
+         10.10,  10.30,  10.50,  10.70,  10.90,  11.10,
+         11.30,  11.50,  11.70,  11.90,  12.10,  12.30,
+         12.50,  12.70
     ]
 
-    var f0: Double {
+    var centralFrequency: Double {
         get {
             return samplingFrequencyHertz
         }
     }
     var lambda: Double {
         get {
-            return self.speedOfUltrasound / (1.0 * self.f0)
+            return self.speedOfUltrasound / (1.0 * self.centralFrequency)
         }
     }
 
@@ -113,8 +104,8 @@ class VerasonicsDelay: NSObject {
                     zs[i] = imageZStartInMM + Double(i + 1) * self.imageZPixelSpacing
                 }
 
-                self.calculatedDelayValues = [[Double]](count: self.numberOfTransducerElements, repeatedValue: [Double]())
-                for (var element = 0; element < self.numberOfTransducerElements; element++) {
+                self.calculatedDelayValues = [[Double]](count: self.numberOfActiveTransducerElements, repeatedValue: [Double]())
+                for (var element = 0; element < self.numberOfActiveTransducerElements; element++) {
                     print("Initializing \(self.imageXPixelCount * self.imageZPixelCount) delays for element \(element + 1)")
                     for(var x = 0; x < self.imageXPixelCount; x += 1) {
                         for (var z = 0; z < self.imageZPixelCount; z += 1) {
@@ -163,9 +154,10 @@ class VerasonicsDelay: NSObject {
         var IQData: [[Complex<Double>]]?
         if let channelData = frame?.channelData {
             let numberOfElements = channelData.count
+            let numberOfSamples = channelData.first?.count
             IQData = [[Complex<Double>]](count: numberOfElements, repeatedValue: [Complex<Double>]())
             for element in 0 ..< numberOfElements {
-                for var sample = 0; sample < 800; sample += 2 {
+                for var sample = 0; sample < numberOfSamples; sample += 2 {
                     /*Getting the IQ data, the first sample is the real sample, the second sample is the
                     complex*/
                     let real: Double = Double(channelData[element][sample])
@@ -192,7 +184,8 @@ class VerasonicsDelay: NSObject {
                     let x_n1 = Int(ceil(self.delays![element][sample]))
                     if (x_n < data[element].count && x_n1 < data[element].count) {
                         let alpha = Double(x_n1) - self.delays![element][sample]
-                        let partA = exp((2.i * M_PI * self.f0 * self.delays![element][sample]) / self.samplingFrequencyHertz)
+                        // Shift frequency by reintroducing the carrier signal
+                        let partA = conj(exp(((2 * M_PI * self.centralFrequency * self.delays![element][sample]) / self.samplingFrequencyHertz).i))
                         let partB = alpha * data[element][x_n - 1] + (1.0 - alpha) * data[element][x_n1 - 1]
                         let interpolatedImageValue = interpolatedImage[sample] + partA * partB
                         interpolatedImage[sample] = interpolatedImageValue
