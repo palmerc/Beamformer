3205448ea05028063d185df837ed439e411b2a82
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-03-05 17:27:50 +0100

    Added a Beamformer parameter to carry static values across the wall

---
+++ Beamformer/Beamformer/ComplexNumber/BeamformerParameters.h
@@ -0,0 +1,12 @@
+#ifndef BeamformerParameters_h
+#define BeamformerParameters_h
+
+
+
+typedef struct BeamformerParameters {
+    long channelCount;
+    long samplesPerChannel;
+    long pixelCount;
+} BeamformerParameters;
+
+#endif
