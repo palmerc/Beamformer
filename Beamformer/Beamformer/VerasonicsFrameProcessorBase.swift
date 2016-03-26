56513e1f64613a41e805b8c9d851dfd4d9f25c05
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-03-26 14:46:44 +0100

    Adjusted the range and renamed the speed of ultrasound in mm per second

--- Beamformer/Beamformer/VerasonicsFrameProcessorBase.swift
+++ Beamformer/Beamformer/VerasonicsFrameProcessorBase.swift
@@ -4,13 +4,13 @@ import Foundation
 
 public class VerasonicsFrameProcessorBase: NSObject
 {
-    let speedOfUltrasound: Float = 1540 * 1000
+    let speedOfUltrasoundInMMPerSecond: Float = 1540 * 1000
     let samplingFrequencyHz: Float = 7813000
     let lensCorrection: Float = 14.14423409
     let numberOfTransducerElements: Int = 192
     let numberOfActiveTransducerElements: Int = 128
-    let imageZStartInMM: Float = 0.0
-    let imageZStopInMM: Float = 50.0
+    let imageZStartInMM: Float = 3.0
+    let imageZStopInMM: Float = 42.5
     var samplesPerChannel: Int = 512
 
     var x_ns: [Int]? = nil
@@ -24,7 +24,7 @@ public class VerasonicsFrameProcessorBase: NSObject
     }
     var lambda: Float {
         get {
-            return self.speedOfUltrasound / (1.0 * self.centralFrequency)
+            return self.speedOfUltrasoundInMMPerSecond / (1.0 * self.centralFrequency)
         }
     }
     var imageXPixelSpacing: Float {
