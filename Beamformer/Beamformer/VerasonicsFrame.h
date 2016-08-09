aaf6a034e0136a36ac56e167aabfefcc4c6c44e4
Cameron Lowell Palmer <cameron.palmer@icloud.com>
2016-08-09 14:30:55 +0200

    Add the timestamp, lens correction and sampling frequency to the VerasonicsFrame class

--- Beamformer/Beamformer/VerasonicsFrame.h
+++ Beamformer/Beamformer/VerasonicsFrame.h
@@ -4,7 +4,9 @@
 
 @interface VerasonicsFrame : NSObject
 @property (assign, nonatomic) NSInteger identifier;
-@property (assign, nonatomic) NSInteger timestamp;
+@property (assign, nonatomic) NSTimeInterval timestamp;
+@property (assign, nonatomic) double lensCorrection;
+@property (assign, nonatomic) double samplingFrequency;
 @property (assign, nonatomic) int32_t numberOfChannels;
 @property (assign, nonatomic) int32_t numberOfSamplesPerChannel;
 @property (assign, nonatomic, readonly) int32_t numberOfSamples;
